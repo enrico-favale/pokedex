9d7891a283489880b0d628a2224a19aa05810b05
enrico-favale <enrico.favale@edu.unife.it>
2024-10-14 00:35:47 +0200

    Defined main page title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='font-courier bg-red-800 min-h-screen text-gray-200 '>
+      <header className='items-center flex justify-center p-4'>
+        <h1 className='text-3xl font-bold'>
+          Pokedex
+        </h1>
+      </header>
+
+      <main className=''>
+      
+      </main>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'courier': ['"Courier New"', 'monospace'], // Aggiungi la font-family qui
+      },
+    },
   },
   plugins: [],
 }
