15a8040fe4045c08b08e3d515af922bde1782453
enrico-favale <enrico.favale@edu.unife.it>
2025-01-09 00:07:21 +0100

    progress bar for stats

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -68,10 +68,10 @@ const PokemonDetail = () => {
             <p className="text-2xl border-b border-txt_secondary mb-2">Base stats</p>
             <div className="max-w-full border-l border-txt_secondary px-2 gap-1">
               {pokemon.stats.map((stat) => (
-                <div key={stat.stat.name} className="grid grid-cols-[minmax(180px,_auto)_minmax(30px,_auto)_1fr] justify-between mb-1 pr-2">
+                <div key={stat.stat.name} className="grid grid-cols-[minmax(180px,_auto)_minmax(40px,_auto)_1fr] justify-between mb-1 pr-2">
                   <p className="text-l">{stat.stat.name.toUpperCase()}:</p>
                   <p className="text-l border-r border-txt_secondary">{stat.base_stat}</p>
-                  <div className="border border-txt_secondary ml-2 m-1 rounded" style={{ width: `${(stat.base_stat / 255) * 100}%` }}></div>
+                  <div className="border border-txt_secondary w-full ml-2 m-1 rounded"><div className="bg-txt_secondary h-full" style={{ width: `${(stat.base_stat / 255) * 100}%` }}></div></div>
                 </div>
               ))}
               <p className="border border-txt_secondary max-w-fit rounded p-1 mt-2">TOTAL: {pokemon.stats.reduce((total, stat) => total + stat.base_stat, 0)}</p>
